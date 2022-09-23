5d8fa872fb34d3d36a5b7a1bb4684fa97e97ca7c
Muhammad Ibrahim <ibrahimraju347@gmail.com>
2022-09-23 15:29:10 +0500

    initial

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import Home from "./screens/home/Home";
+import Header from "./common/header/Header";
+import { RadalChart } from "./common/RadalChart";
+
+function App() {
+  return (
+    <div>
+      {/* <Header />
+      <Home /> */}
+      <RadalChart />
+    </div>
+  );
+}
+
+export default App;
